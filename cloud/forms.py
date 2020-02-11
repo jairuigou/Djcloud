acd4adab971ae699eb8880c689b2a8dde6990f22
jairuigou <jairuigou@gmail.com>
2020-02-11 20:42:13 +0800

    add forms

--- cloud/forms.py
+++ cloud/forms.py
@@ -5,6 +5,6 @@ class LoginForm(forms.Form):
     username = forms.CharField(label='username',max_length=30)
     password = forms.CharField(label='password',
                                 max_length=30,
-                                widget=widgets.PasswordInput(),
-                                error_messages={'required':'Enter a password'})
+            widget=widgets.PasswordInput(attrs={'oninvalid':"setCustomValidity('Enter a password')"}))
+    
     
