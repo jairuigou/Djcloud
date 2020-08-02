9e00d642823b968904604e30721be69fd4e6eb79
jairuigou <jairuigou@gmail.com>
2020-08-02 14:53:37 +0800

    feat(usercard): admin UserCard

--- front/src/pages/admin/components/UserCard.vue
+++ front/src/pages/admin/components/UserCard.vue
@@ -1,12 +1,43 @@
 <template>
-    <el-card shadow="hover" class = "usercard" :body-style="{ padding: '0px' }">
-        <!--<i class ="el-icon-plus"></i>-->
-        <img src="../../../assets/3.jpg" class = "image">
-        <div>UserName</div>
-    </el-card>    
+    <el-card shadow="hover" class = "usercard" :body-style="{ padding: '0px'}">
+        <div v-if="hasInfo == true">
+            <img id="avatarimg" src="../../../assets/3.jpg" class="image">
+            <div style="padding : 10px">
+                <span >{{info.UserName}}</span>
+                <div class="greencircle" v-if="info.Status=='online'"></div>
+                <div v-if="info.Status!='online'">{{info.LastLoginTime}}</div>
+                <el-progress class="usage" :stroke-width="10" :show-text="false" 
+                    :percentage="info.DiskUsage/info.DiskAllocation*100"></el-progress>
+            </div>
+        </div>
+        <div v-else>
+            add an user
+        </div>
+    </el-card>
 </template>
 <script>
 export default {
+    data(){
+        return{
+            hasInfo: true,
+            info:{
+                'UserName':'jairui',
+                'Status':'online',
+                'LastLoginTime':'2020/05/05',
+                'DiskUsage':'10',
+                'DiskAllocation':'20',
+                'AvatarLink':'None'
+            }
+        }
+    },
+    mounted:function(){
+        if(info.AvatarLink != "None"){
+            var obj = document.
+        }
+    },
+    methods:{
+
+    }
     
 }
 </script>
@@ -15,11 +46,26 @@ export default {
     font-size: 150px;
 }
 .image{
-    width: 100%;
+    width:100%;
+    height:180px;
     display: block;
 }
 .usercard{
     width:200px;
-    height: 230px;
+    height: 250px;
+    border-radius: 3%;
 }
+.greencircle{
+    margin: 8px;
+    display:inline-block;
+    width:6px;
+    height:6px;
+    background: #67C23A;
+    border-radius: 50%;
+}
+.usage{
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
 </style>
